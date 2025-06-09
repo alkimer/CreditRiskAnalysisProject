a8edae0eddc4421364ccba377ae1cebebd0f6a6e
Andres_Amaya_Chaves <andresamayachaves@gmail.com>
2025-06-09 16:10:09 -0500

    Advanced with the processing 1

--- processing/process_categorical_binary.py
+++ processing/process_categorical_binary.py
@@ -11,7 +11,9 @@ def start():
     y_val_full   = pd.read_csv('./data/data_splitted/y_val.csv',   header=None, encoding='UTF-8') #delimiter=','
     
     var_names = pd.read_csv('./external/var_names.txt', header=None, encoding='UTF-8') # delimiter=','
-    binary_index = [6,16,33,37] # + Others, review twice or more!!!!
+    blurry = [5,20,21,24,25,26,27,28]
+    binary_index = [6,16,33,37] # + Others, review twice or more!!!!    
+    binary_index.append(blurry).sort()
     
     print("The following are the binary categrorical variables \nin the dataset (first their indexes):")
     display(binary_index
