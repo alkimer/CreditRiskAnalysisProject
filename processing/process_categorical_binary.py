f8891979e544fe477f2dd1a6da030e60fb4ad018
Andres_Amaya_Chaves <andresamayachaves@gmail.com>
2025-06-09 19:39:26 -0500

    Advancement 2

--- processing/process_categorical_binary.py
+++ processing/process_categorical_binary.py
@@ -1,25 +1,50 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy.stats import chi2_contingency
-import numpy as np
+
 
 def start():
     X_train_full = pd.read_csv('./data/data_splitted/X_train.csv', header=None, encoding='UTF-8')
     X_val_full   = pd.read_csv('./data/data_splitted/X_val.csv',   header=None, encoding='UTF-8')
-    y_train_full = pd.read_csv('./data/data_splitted/y_train.csv', header=None, encoding='UTF-8')
-    y_val_full   = pd.read_csv('./data/data_splitted/y_val.csv',   header=None, encoding='UTF-8') #delimiter=','
-    
-    var_names = pd.read_csv('./external/var_names.txt', header=None, encoding='UTF-8') # delimiter=','
-    blurry = [5,20,21,24,25,26,27,28]
-    binary_index = [6,16,33,37] # + Others, review twice or more!!!!    
-    binary_index.append(blurry).sort()
-    
-    print("The following are the binary categrorical variables \nin the dataset (first their indexes):")
-    display(binary_index
-            )
+    var_names    = pd.read_csv('./external/var_names.txt', header=None, delimiter='\t', encoding='latin1')
+
+    blurry = [5,21,24,25,26,27,28]
+    binary_index = [6,16,33,37]
+    binary_index.extend(blurry)
+    binary_index.sort()
+
+    print("The following are the binary categrorical variables\nin the dataset (first their indexes):\n")
+    print(binary_index)
+    display(var_names[binary_index])
+
     # Create binary subdatasets
-    X_train = X_train_full[binary_index]
-    X_val = X_val_full[binary_index]
-    y_train = y_train_full[binary_index]
-    y_val = y_val_full[binary_index]
+    X_train = X_train_full.iloc[:,binary_index]
+    X_val = X_val_full.iloc[:,binary_index]
+    
+
+def process_POSTAL_ADDRESS_TYPE():    
+    return
+
+def process_SEX():
+    return
+
+def process_FLAG_RESIDENCIAL_PHONE():
+    return
+
+def process_FLAG_EMAIL():
+    return
+
+def process_FLAG_VISA():
+    return
+
+def process_FLAG_MASTERCARD():
+    return
+
+def process_FLAG_DINERS():   
+    return
+
+
+def process_FLAG_FLAG_AMERICAN_EXPRESS():
+    return
+
+def process_FLAG_PROFESSIONAL_PHONE():
+    return
