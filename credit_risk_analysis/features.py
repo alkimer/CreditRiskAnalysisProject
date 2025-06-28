420979aba7ff27ab77bdfb8c4ccf3feb70b280f2
Andres Galarza <51804117+andresgalarza-astro@users.noreply.github.com>
2025-06-28 18:36:05 -0500

    Add files via upload

--- credit_risk_analysis/features.py
+++ credit_risk_analysis/features.py
@@ -4,119 +4,180 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from credit_risk_analysis.config import RAW_DATA_DIR, PROCESSED_DATA_DIR
+from credit_risk_analysis.config import RAW_DATA_DIR, INTERIM_DATA_DIR, PROCESSED_DATA_DIR
 
-import os
+from matplotlib.pyplot import grid
 import pandas as pd
 import numpy as np
+import json
+import os
+import joblib
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.feature_selection import VarianceThreshold, SelectKBest, chi2, f_classif, mutual_info_classif
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+from imblearn.over_sampling import SMOTE
 
 app = typer.Typer()
 
-def normalize_categorical_columns(df):
-    """
-    Normalize categorical columns in the DataFrame.
-    This function should be implemented based on the specific requirements of the dataset.
-    """
-    # Example implementation (to be replaced with actual logic)
-    for col in df.select_dtypes(include=['object']).columns:
-        df[col] = df[col].str.lower().str.strip()
-    return df
+# Frequency Encoder personalizado
+class FrequencyEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.freq_maps = {}
+        self.feature_names_in_ = None
 
-def top_n_label_encoder(df, n=10):
-    """
-    Encode categorical columns using top-n label encoding.
-    This function should be implemented based on the specific requirements of the dataset.
-    """
-    # Example implementation (to be replaced with actual logic)
-    for col in df.select_dtypes(include=['object']).columns:
-        top_n = df[col].value_counts().nlargest(n).index
-        df[col] = df[col].where(df[col].isin(top_n), other='other')
-        df[col] = df[col].astype('category').cat.codes
-    return df
+    def fit(self, X, y=None):
+        if isinstance(X, np.ndarray):
+            X = pd.DataFrame(X)
+        self.feature_names_in_ = X.columns
+        for col in self.feature_names_in_:
+            freq = X[col].value_counts(normalize=True)
+            self.freq_maps[col] = freq
+        return self
 
-def select_features(X_train, y_train, selection_method=VarianceThreshold(threshold=0.01), k=15):
-    """
-    Select relevant features from the DataFrame.
-    This function should be implemented based on the specific requirements of the dataset.
-    """
-    # Example implementation (to be replaced with actual logic)
-    categorical_columns = X_train.select_dtypes(include=['object', 'category']).columns
-    numerical_columns = X_train.select_dtypes(include=['int64', 'float64']).columns
-    
-    # Define preprocessing for numerical and categorical features
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', Pipeline([
-                ('imputer', SimpleImputer(strategy='mean')),  # Impute missing values with mean for numerical features
-                #('scaler', StandardScaler())  # Scale numerical features
-            ]), numerical_columns),
-            ('cat', Pipeline([
-                ('imputer', SimpleImputer(strategy='most_frequent')),  # Impute missing values with mode for categorical features
-                ('encoder', OneHotEncoder(handle_unknown='ignore'))  # Encode categorical features
-            ]), categorical_columns)
-        ]
-    )
+    def transform(self, X):
+        check_is_fitted(self, 'freq_maps')
+        if isinstance(X, np.ndarray):
+            X = pd.DataFrame(X, columns=self.feature_names_in_)
+        X_encoded = X.copy()
+        for col in self.feature_names_in_:
+            X_encoded[col] = X_encoded[col].map(self.freq_maps[col]).fillna(0)
+        return X_encoded.to_numpy()
 
-    pipeline = Pipeline([
-        ('preprocessing', preprocessor),  # Preprocess the data
-        ('feature_selection', selection_method), 
-    ])
 
-    # Fit the pipeline on the training data
-    pipeline.fit(X_train, y_train.values.ravel())
+def process_features(X_new_path=None,new_data=False, return_data=False):
+    # Columnas
+    vars_to_drop = [
+        'ID_CLIENT', 'CLERK_TYPE', 'QUANT_ADDITIONAL_CARDS', 'POSTAL_ADDRESS_TYPE',
+        'EDUCATION_LEVEL', 'FLAG_MOBILE_PHONE', 'FLAG_DINERS', 'FLAG_AMERICAN_EXPRESS',
+        'QUANT_SPECIAL_BANKING_ACCOUNTS', 'COMPANY', 'PROFESSIONAL_CITY',
+        'PROFESSIONAL_BOROUGH', 'MONTHS_IN_THE_JOB', 'MATE_PROFESSION_CODE',
+        'FLAG_HOME_ADDRESS_DOCUMENT', 'FLAG_RG', 'FLAG_CPF', 'FLAG_INCOME_PROOF',
+        'FLAG_ACSP_RECORD']
 
-    # Get the selected feature names
-    selected_features = pipeline.named_steps['feature_selection'].get_support(indices=True)
-    preprocessed_feature_names = pipeline.named_steps['preprocessing'].get_feature_names_out()
-    selected_feature_names = preprocessed_feature_names[selected_features]
-    print("Selected Features:", selected_feature_names)
+    num_cont = ['PERSONAL_MONTHLY_INCOME', 'OTHER_INCOMES', 'PERSONAL_ASSETS_VALUE']
+    num_disc = ['MARITAL_STATUS', 'QUANT_DEPENDANTS', 'MONTHS_IN_RESIDENCE',
+                'QUANT_BANKING_ACCOUNTS', 'QUANT_CARS', 'AGE',
+                'PROFESSION_CODE', 'OCCUPATION_TYPE']
+    cat_bin = ['APPLICATION_SUBMISSION_TYPE', 'SEX', 'FLAG_RESIDENCIAL_PHONE', 'FLAG_EMAIL',
+               'FLAG_VISA', 'FLAG_MASTERCARD', 'FLAG_OTHER_CARDS', 'FLAG_PROFESSIONAL_PHONE']
+    cat_multi = ['STATE_OF_BIRTH', 'CITY_OF_BIRTH', 'RESIDENCIAL_STATE', 'RESIDENCIAL_CITY',
+                 'RESIDENCIAL_BOROUGH', 'RESIDENCIAL_PHONE_AREA_CODE', 'RESIDENCE_TYPE',
+                 'PROFESSIONAL_STATE', 'PROFESSIONAL_PHONE_AREA_CODE', 'NACIONALITY',
+                 'PRODUCT', 'RESIDENCIAL_ZIP_3', 'PROFESSIONAL_ZIP_3']
 
-    return selected_feature_names
+    if new_data==False:
+        # Cargar datos desde la carpeta correcta
+        X_train = pd.read_csv("data/interim/X_train.csv")
+        X_val = pd.read_csv("data/interim/X_val.csv")
+        y_train = pd.read_csv("data/interim/y_train.csv").select_dtypes(include='number').iloc[:, 0].astype(int)
+        y_val = pd.read_csv("data/interim/y_val.csv").select_dtypes(include='number').iloc[:, 0].astype(int)
+
+        
+        # Eliminar columnas
+        X_train.drop(columns=vars_to_drop, inplace=True, errors='ignore')
+        X_val.drop(columns=vars_to_drop, inplace=True, errors='ignore')
+
+        # Separar multicategóricas
+        multi_high_card = [col for col in cat_multi if X_train[col].nunique() > 30]
+        multi_low_card = [col for col in cat_multi if col not in multi_high_card]
+
+        # Pipelines
+        num_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', StandardScaler())
+        ])
+
+        cat_bin_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('encode', OneHotEncoder(drop='if_binary', sparse_output=False))
+        ])
+
+        cat_multi_low_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
+        ])
+
+        cat_multi_high_pipeline = Pipeline([
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('freq', FrequencyEncoder())
+        ])
+
+        # ColumnTransformer
+        preprocessor = ColumnTransformer(transformers=[
+            ('num', num_pipeline, num_cont + num_disc),
+            ('bin', cat_bin_pipeline, cat_bin),
+            ('multi_low', cat_multi_low_pipeline, multi_low_card),
+            ('multi_high', cat_multi_high_pipeline, multi_high_card)
+        ])
+
+        # Procesamiento
+        X_train_processed = preprocessor.fit_transform(X_train)
+        X_val_processed = preprocessor.transform(X_val)
+
+        # Columnas finales
+        bin_names = preprocessor.named_transformers_['bin']['encode'].get_feature_names_out(cat_bin)
+        multi_low_names = preprocessor.named_transformers_['multi_low']['onehot'].get_feature_names_out(multi_low_card)
+        multi_high_names = multi_high_card
+        final_columns = num_cont + num_disc + list(bin_names) + list(multi_low_names) + multi_high_names
+
+        with open('data/interim/final_columns.json', 'w') as file:
+            json.dump(final_columns, file)
+
+        X_train_final = pd.DataFrame(X_train_processed, columns=final_columns, index=X_train.index)
+        X_val_final = pd.DataFrame(X_val_processed, columns=final_columns, index=X_val.index)
+
+        #os.makedirs("data/processed/balanced", exist_ok=True)
+        # Guardar preprocessor
+        joblib.dump(preprocessor, 'models/preprocessor.pkl', compress=True)
+
+        # Guardar data procesada
+        X_train_final.to_csv("data/processed/X_train_processed.csv", index=False)
+        X_val_final.to_csv("data/processed/X_val_processed.csv", index=False)
+        y_train.to_csv("data/processed/y_train.csv", index=False)
+        y_val.to_csv("data/processed/y_val.csv", index=False)
+
+        # Aplicar SMOTE
+        smote = SMOTE(random_state=42)
+        X_train_final_bal, y_train_bal = smote.fit_resample(X_train_final, y_train)
+        
+        X_train_final_bal.to_csv("data/processed/X_train_processed_bal.csv", index=False)
+        y_train_bal.to_csv("data/processed/y_train_bal.csv", index=False)
+
+        print("✅ Archivos procesados y guardados correctamente.")
+    else:
+        X_new = pd.read_csv(X_new_path)
+        X_new.drop(columns=vars_to_drop, inplace=True, errors='ignore')
+        # Separar multicategóricas
+        multi_high_card = [col for col in cat_multi if X_new[col].nunique() > 30]
+        multi_low_card = [col for col in cat_multi if col not in multi_high_card]
+
+        # cargar preprocessor previamente ajustado
+        preprocessor = joblib.load('models/preprocessor.pkl')
+        # Procesamiento
+        X_new_processed = preprocessor.transform(X_new)
+        # Columnas finales
+        with open('data/interim/final_columns.json', 'r') as file:
+            final_columns = json.load(file)
+
+        X_new_final = pd.DataFrame(X_new_processed, columns=final_columns, index=X_new.index)
+        #os.makedirs("data/processed/balanced", exist_ok=True)
+
+        # Guardar data procesada
+        X_new_final.to_csv("data/processed/X_new_processed.csv", index=False)
+        print("✅ Nuevos datos procesados y guardados correctamente.")
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR,
-    output_path: Path = PROCESSED_DATA_DIR,
-    stat_model_option: str = 'VarianceThreshold',  # Options: 'VarianceThreshold', 'SelectKBest_chi2', 'SelectKBest_f_classif', 'SelectKBest_mutual_info_classif'
-    Threshold: float = 0.01,
-    K: int = 15,
-    # -----------------------------------------
+        X_new_path=None,
+        new_data=False
 ):
-    # -----------------------------------------
-    logger.info("Loading training data...")
-    x_train_path = os.path.join(input_path, "X_train.csv")
-    y_train_path = os.path.join(input_path, "y_train.csv")
-    X_train = pd.read_csv(x_train_path)
-    y_train = pd.read_csv(y_train_path)
-    logger.success("Loading training data complete.")
-    X_train = X_train.drop(columns=['ID_CLIENT', 'CLERK_TYPE'], errors='ignore')
-    X_train = normalize_categorical_columns(X_train)
-    X_train = top_n_label_encoder(X_train, n=10)
-    logger.info("Setting up feature selection method...")
-    if stat_model_option == 'VarianceThreshold':
-        selection_method = VarianceThreshold(threshold=Threshold)
-    elif stat_model_option == 'SelectKBest_chi2':
-        selection_method = SelectKBest(chi2, k=K)
-    elif stat_model_option == 'SelectKBest_f_classif':
-        selection_method = SelectKBest(f_classif, k=K)
-    elif stat_model_option == 'SelectKBest_mutual_info_classif':
-        selection_method = SelectKBest(mutual_info_classif, k=K)
-    else:
-        logger.error("Invalid selection method. Defaulting to VarianceThreshold.")
-        selection_method = VarianceThreshold(threshold=0.01)
-    logger.success("Feature selection method set to: {}".format(selection_method))
-    logger.info("Starting feature selection...")
-    selected_features = select_features(X_train, y_train, selection_method=selection_method)
-    logger.success("Features selection complete.")
-    # -----------------------------------------
-    return selected_features
 
+    print("Iniciando procesamiento de características...")
+    process_features(X_new_path=X_new_path, new_data=new_data)
+    print("Procesamiento completado.")
 
 if __name__ == "__main__":
-    app()
+    main()
